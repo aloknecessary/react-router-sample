9e00337626b4ee89623ddb69eb51ccc9f8792dec
Alok Ranjan <aloks@byteridge.com>
2023-05-02 13:52:52 +0530

    package updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {
-  Route,
-  Link,
-  Switch,
-  Redirect
-} from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Link, Switch, Redirect } from "react-router-dom";
 
-import RedirectOnClickSampler from './components/clickRedirectSampler';
-import About from './components/about';
-import Messages from './components/message';
+import RedirectOnClickSampler from "./components/clickRedirectSampler";
+import About from "./components/about";
+import Messages from "./components/message";
 
 const Home = () => (
   <div>
     <h2>Home</h2>
     This example shows how to use React Router. Welcome to Home!!
     <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
+      <img src={logo} className="App-logo" alt="logo" />
+      {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>  <a
           className="App-link"
@@ -28,31 +23,34 @@ const Home = () => (
         >
           Learn React
         </a> */}
-      </header>
+    </header>
   </div>
 );
-
-
 
 function App() {
   return (
     <div className="App">
-      
       <div className="menu">
-            <ul>
-              <li> <Link to="/messages">Messages</Link> </li>
-              <li> <Link to="/about">About</Link> </li>
-            </ul>
-            <RedirectOnClickSampler />
-        </div>
-        <div className="App-intro">
-          <Switch>
-            <Route exact path="/"  component={Home} />
-            <Route path="/messages" component={Messages} />
-            <Route path="/about" component={About} />
-            <Redirect to="/" />
-          </Switch>
-        </div>
+        <ul>
+          <li>
+            {" "}
+            <Link to="/messages">Messages</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to="/about">About</Link>{" "}
+          </li>
+        </ul>
+        <RedirectOnClickSampler />
+      </div>
+      <div className="App-intro">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/messages" component={Messages} />
+          <Route path="/about" component={About} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
     </div>
   );
 }
